d9f0ceb58b710fffe59eed3809a722790b26d217
Lautaro Manuel Orsi <lautaorsi@gmail.com>
2025-04-06 20:54:51 -0300

    masking & bitshift

--- Ej9/low-high.c
+++ Ej9/low-high.c
@@ -2,9 +2,12 @@
 #include <stdint.h>
 
 int main(){
-    uint32_t high_mask = 0x7;
-    uint32_t low_mask = 0xE0000000;
-    printf("%lu", (high_mask & low_mask));
+    uint32_t low_mask = 0x7;
+    uint32_t high_mask = 0xE0000000;
+    uint32_t word1, word2;
+    scanf("%x%x", &word1, &word2);
+    printf("%d",((word1 & high_mask) >> 29) == (word2 & low_mask));
+
 
 
     return 0;
